2eb7f8cad4cdc6dcad24f293efb706f1956a64d4
UDJAT74 <128726786+UDJAT74@users.noreply.github.com>
2023-06-05 18:54:03 +0300

    Add files via upload

--- detector.py
+++ detector.py
@@ -3,22 +3,22 @@ import json
 from ultralytics import YOLO
 from ultralytics.yolo.utils.plotting import Annotator
 
+model = YOLO("bestface.pt")
+model2 = YOLO("weapondetection.pt")
+recognizer = cv.face.LBPHFaceRecognizer_create()
+recognizer.read('trainner.yml')
+
+#open users json file and retrieve names
+with open('users.json') as jsonFile:
+    users = json.load(jsonFile)
+userList = []
 def detector():
-    model = YOLO("bestface.pt")
-
     camera = cv.VideoCapture(0)
-
     # Capture video on webcam
     count = 0
 
     #Detect face and recognize 
-    recognizer = cv.face.LBPHFaceRecognizer_create()
-    recognizer.read('trainner.yml')
 
-    #open users json file and retrieve names
-    with open('users.json') as jsonFile:
-        users = json.load(jsonFile) 
-    userList = []
     #loop over the frames 
     while 1:
         #read frames 
@@ -35,16 +35,15 @@ def detector():
                 c = box.cls
                 # annotator.box_label(b, model.names[int(c)], 3)
                 x, y, w, h = int(b[0]), int(b[1]), int(b[2]), int(b[3])
-                # cv.rectangle(image, (x, y), (x + w, y + h), (100, 100, 0), 2)
-
-                cv.rectangle(image, (x,y), (w, h), (100,0,100), 2)
+                cv.rectangle(image, (x, y), (x + w, y + h), (100, 100, 0), 2)
+                # cv.rectangle(image, (x,y), (w, h), (100,0,100), 2)
 
                 faceId, percentage = recognizer.predict(gray[y:y+h, x:x+w])
                 print(faceId, percentage)
-
-                if percentage < 50 and faceId != 1:
+                faceId=str(faceId)
+                if percentage < 50 :
                     userList.append(faceId)
-                    faceId = users[str(faceId)]['name']+' '+str(round(100-percentage,2))+"%"
+                    faceId = users[str(faceId)]['name']
                 else:
                     faceId = 'Unknown'
 
@@ -56,7 +55,56 @@ def detector():
     camera.release()
     cv.destroyAllWindows()
 
-# detector()
+def live_detection():
 
+    camera = cv.VideoCapture(0)
+    while True:
+        res, frame = camera.read()
+        if not res:
+            break
+        else:
+            # detect weapon
+            result = model2.predict(frame)
+            for r in result:
+                annotator = Annotator(frame)
+                boxes = r.boxes
+                for box in boxes:
+                    b = box.xyxy[0]
+                    c = box.cls
+                    annotator.box_label(b, model2.names[int(c)], 3)
+            #detect face
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+            height, width = frame.shape[:2]
+            result = model.predict(frame)
+            for r in result:
+                annotator = Annotator(frame)
+                boxes = r.boxes
+                for box in boxes:
+                    b = box.xyxy[0]
+                    c = box.cls
+                    annotator.box_label(b, model.names[int(c)], 3)
+                    x, y, w, h = int(b[0]), int(b[1]), int(b[2]), int(b[3])
+                    # cv.rectangle(frame, (x, y), (x + w, y + h), (100, 100, 0), 2)
+                    # cv.rectangle(frame, (x,y), (w, h), (100,0,100), 2)
+
+                    faceId, percentage = recognizer.predict(gray[y:y + h, x:x + w])
+                    print(faceId, percentage)
+                    faceId = str(faceId)
+                    if percentage < 50:
+                        userList.append(faceId)
+                        faceId = users[str(faceId)]['name']
+                    else:
+                        faceId = 'Unknown'
+
+                    cv.putText(frame, faceId, (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, (50, 255,), 2)
+
+        cv.imshow('Image', frame)
+        if cv.waitKey(20) & 0xFF == ord('q'):
+            break
+    camera.release()
+    cv.destroyAllWindows()
+# detector()
+# live_detection()
 
 
